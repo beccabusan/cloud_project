4460b48d3ac17364c3a0b48c0aa530c1d8a3b5f7
nogna <albin.sundqvist93@gmail.com>
2016-10-12 12:10:34 +0000

    Working for 1,2

--- svmscript.py
+++ svmscript.py
@@ -1,32 +1,36 @@
 from celery import Celery
 import os, sys, subprocess, time 
-from os import environment as env
-GEODIR = "/geo/"
+from os import environ as env
+geofilepath = "/home/ubuntu/cloud_project/geo"
+mshfilepath ="/home/ubuntu/cloud_project/msh"
 
-username = env['CEL_USER_NAME']
-userpwd = env['CEL_USER_PWD']
-masterip = env['CEL_MASTER_IP']
-mastervhost = env['CEL_MASTER_VHOST']
 
-mainurl = "amqp:// " + username + ":" + userpwd + "@" + masterip + "/" + mastervhost
+
+username = "slavevm1"
+userpwd = "slavepwd1"
+masterip = "127.0.0.1"
+mastervhost = "mastervhost"
+
+mainurl = "amqp://" + username + ":" + userpwd + "@" + masterip + "/" + mastervhost
 
 app = Celery('fenicstask', backend=mainurl, broker=mainurl)
 
 @app.task
 def work(a_start, a_stop, n_angles, n_nodes, n_levels):
-	strcmd = "./runme.sh " + a_start + " " + a_stop + " " + n_angles + " " + n_nodes + " " + n_levels
-	meshfiles = subprocess.call(['./runme.sh','a_start','a_stop','n_angles','n_nodes','n_levels'])
+#	strcmd = "./run.sh " + a_start + " " + a_stop + " " + n_angles + " " + n_nodes + " " + n_levels
+	meshfiles = subprocess.call(['./run.sh',str(a_start),str(a_stop),str(n_angles),str(n_nodes),str(n_levels)])
 	
-	while meshfiles==0:
-		time.sleep(0.1)
-	geofilepath = GEODIR
 
-	for file in geofilepath:
-		geofilename = os.path.basename(file))
-		xmlfilename = ((geofilename).split('.'))[0] + ".xml"
-    	convert = subprocess.call(['./dolphin-convert', geofilename, xmlfilename])
+	for file in os.listdir(mshfilepath):
+		print file		
+		mshfilename = "/home/ubuntu/cloud_project/msh/"+file
+		print mshfilename
+		xmlfilename = ((mshfilename).split('.'))[0] + ".xml"
+		print xmlfilename
+              	convert = subprocess.call(['dolfin-convert', mshfilename, xmlfilename])
 	
-    return "worked."
+	return "worked."
+
 
 
 
