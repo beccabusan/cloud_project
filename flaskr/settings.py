689c55725fd0a5cf51ab38c317944c843721adda
root <rebecca.oskarsson@live.se>
2016-10-15 07:03:29 +0000

    added UI and flask app

---
+++ flaskr/settings.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'not_a_secret'
+CELERY_BROKER_URL='amqp://guest@localhost'
+CELERY_RESULT_BACKEND='amqp://guest@localhost'
---
+++ flaskr/static/js/functions.js
@@ -0,0 +1,4 @@
+function go(){
+    $.ajax({
+    url:"http://130.238.29.62:5000/readtweets"
+}
