160c32389ad63c61c57b45c3b0311b3cf3c3f501
nogna <albin.sundqvist93@gmail.com>
2016-10-16 04:59:32 -0700

    Added container logic

--- generate_mesh_convert_xml_MASTERVM.py
+++ generate_mesh_convert_xml_MASTERVM.py
@@ -10,6 +10,18 @@ xmlfilepath = homepath+"xml_files/"
 ##Setting the defualt values##
 ##############################
 def generate_convert(a_start="0", a_stop='30', n_angles='5', n_nodes='5', n_levels='0'):
+	
+	swift_con = swiftclient.client.Connection(authurl='http://130.238.29.253:5000/v3',
+                                               user='albins',
+                                               key='grupp6',
+                                               tenant_name='g2015034',
+                                               auth_version='3',
+                                               os_options={'tenant_id':'74833650f49e4227b868610684b155f2' , 						       'region_name': 'UPPMAX'})
+
+	container_name = 'Grupp6'		
+		
+	
+	
 	##Generating the .msh and .geo files
 	meshfiles = subprocess.call(['./run.sh',str(a_start),str(a_stop),str(n_angles),str(n_nodes),str(n_levels)])
 
@@ -21,3 +33,7 @@ def generate_convert(a_start="0", a_stop='30', n_angles='5', n_nodes='5', n_leve
 		print "Converting "+ mshfilename +" into "+ xmlfilename
         	convert = subprocess.call(['dolfin-convert', mshfilename, xmlfilename])
 
+		with open(xmlfilename, 'r') as xmlfiletoUpload:
+        		swift_con.put_object(container_name, xmlfilename,
+                                        contents= xmlfiletoUpload.read(),
+                                        content_type='text/plain')
--- running_AIRFOIL_arg_XML_SLAVEVM.py
+++ running_AIRFOIL_arg_XML_SLAVEVM.py
@@ -6,7 +6,7 @@ from os import environ as env
 homepath = '/home/ubuntu/'
 geofilepath = homepath + "cloud_project/geo"
 mshfilepath = homepath + "cloud_project/msh"
-xmlfilepath = homepath + "xml_files/" ###THIS SHOULD BE THE STANDARD FILE PATH, IE ON THE CONTAINER OR SOMETHING##
+xmldir = homepath + "xml_files/" 
 resultpath= os.getcwd() + "/results/drag_ligt.m"
 
 
@@ -20,13 +20,30 @@ mainurl = "amqp://" + username + ":" + userpwd + "@" + masterip + "/" + mastervh
 app = Celery('fenicstask', backend=mainurl, broker=mainurl)
 
 @app.task
-def work(xmlfile_to_calculate):
-	str_angle = re.search('a(.+?)n', xmlfile_to_calculate)
+def work(xmlfilename, samples=10, viscosity=0.0001, speed=10., time=1):
+			
+	swift_con = swiftclient.client.Connection(authurl='http://130.238.29.253:5000/v3',
+                                               user='albins',
+                                               key='grupp6',
+                                               tenant_name='g2015034',
+                                               auth_version='3',
+                                               os_options={'tenant_id':'74833650f49e4227b868610684b155f2' , 'region_name': 'UPPMAX'})
+
+	container_name = 'Grupp6'
+	xmlfile = swift_con.get_object(container_name, xmlfilename)
+
+	with open(xmldir + xmlfilename, 'w') as xmltoDL:
+        	xmltoDL.write(obj_tuple[1])
+		
+		
+	###Find the angle 
+	str_angle = re.search('a(.+?)n', xmlfilename)
         if str_angle:
 		angle = str_angle.group(1)
                	print "This is the angle: "+ angle
+
 	airfoil_bin_path = "/home/ubuntu/cloud_project/navier_stokes_solver/./airfoil"
-	get_airfoil_result = subprocess.call([airfoil_bin_path, '10', '0.0001','10.','1', xmlfilepath + xmlfile_to_calculate])
+	get_airfoil_result = subprocess.call([airfoil_bin_path, str(samples), str(viscosity), str(speed), str(time), xmldir + xmlfilename)
                
 	print 'Starting some calculations on angle: '+ angle
         dlfile = open(resultpath,'r')
