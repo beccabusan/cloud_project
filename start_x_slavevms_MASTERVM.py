563ea8d2b6e0bed1010976f597716eb9ec7596b6
M <M@Macintosh-5.local>
2016-10-17 21:29:47 +0200

    initvms change

--- start_x_slavevms_MASTERVM.py
+++ start_x_slavevms_MASTERVM.py
@@ -2,15 +2,15 @@
 import os, subprocess
 import sys
 
-
 #def start_x_slavevms():
 if sys.argv[1:]:
+	masterip = subprocess.check_output(['curl','ipecho.net/plain'])
 	number_of_vmslaves = int(sys.argv[1])
 	i=1
 	while i <= number_of_vmslaves:
 		print "Iniated slave number: "+ str(i)
 		###TODO ADD USER DATA FOR EACH WORKER- CELERY SETUP
-		subprocess.call('python starting_script.py ' + str(i), shell=True)				
+		subprocess.call('python starting_script.py ' + str(i) + " " + str(masterip), shell=True)				
 		i += 1
 	
 
