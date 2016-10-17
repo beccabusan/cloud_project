ef74e205c250bc26ce8d9f3c9380082e839040b3
M <M@Macintosh-5.local>
2016-10-17 20:40:26 +0200

    mastersendwork update

--- flaskr/mastersendwork.py
+++ flaskr/mastersendwork.py
@@ -1,12 +1,12 @@
 import sys
 from celery import Celery 
+celery = Celery()
+celery.conf.update('settings')
 from running_AIRFOIL_arg_XML_SLAVEVM import work
 
 if __name__=="__main__":
 	filelist = sys.argv[1]
 	tasks = []
-	celery = Celery()
-	celery.conf.update('settings')
 		for line in filelist:
 			tasks.append(work.delay(line))
 	return tasks		
