46fb26ac716b7baa132eac0f0f01cf999b1e3e13
M <M@nl119-180-4.student.uu.se>
2016-10-18 12:25:49 +0200

    settings

--- flaskr/settings.py
+++ flaskr/settings.py
@@ -1,3 +1,3 @@
 SECRET_KEY = 'not_a_secret'
-CELERY_BROKER_URL='amqp://guest@localhost'
-CELERY_RESULT_BACKEND='amqp://guest@localhost'
+CELERY_BROKER_URL='amqp://mastervm:masterpwd@127.0.0.1/mastervhost'
+CELERY_RESULT_BACKEND='amqp://mastervm:masterpwd@127.0.0.1/mastervhost'
