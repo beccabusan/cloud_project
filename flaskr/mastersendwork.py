ee58e02cfb085a22e73a10dccd457ce5aa2967a4
M <M@nl119-180-4.student.uu.se>
2016-10-18 11:38:03 +0200

    mastersend

--- flaskr/mastersendwork.py
+++ flaskr/mastersendwork.py
@@ -5,7 +5,7 @@ celery.conf.update('settings')
 from running_AIRFOIL_arg_XML_SLAVEVM import work
 
 if __name__=="__main__":
-	filelist = sys.argv[1]
+	filelist = (sys.argv[1]).split()
 	tasks = []
 		for line in filelist:
 			tasks.append(work.delay(line))
